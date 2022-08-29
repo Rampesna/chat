fae669cbea411ea79ba58f5d26316999f185c763
Talha Can <rampesna@gmail.com>
2022-08-29 17:00:35 +0300

    v1.0.0.1

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
+const http = require('http');
 const userRoutes = require('./routes/user');
 const errorsRoutes = require('./routes/errors');
 const envoirments = require('dotenv').config().parsed;
 
 const server = express();
+
+const httpServer = http.createServer(server);
+const {Server} = require("socket.io");
+const socketIoServer = new Server(httpServer);
+httpServer.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
 server.use(express.json());
 server.listen(envoirments.SERVER_PORT);
+
+socketIoServer.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+socketIoServer.on('disconnect', (socket) => {
+    console.log('a user disconnected');
+});
 
 server.use('/user', userRoutes);
 server.use('/', errorsRoutes);
 
-server.get('*', (request, response) => {
-    response.redirect('/404');
-});
-
 server.get('/', (req, res) => {
     res.end('Home Page!');
+});
+
+server.get('*', (request, response) => {
+    response.redirect('/404');
 });
